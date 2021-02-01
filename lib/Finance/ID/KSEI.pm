c09ef9e4ad21288c1c252ce5528fb556f234baee
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2021-02-01 19:39:05 +0700

    Finish first implementation

--- lib/Finance/ID/KSEI.pm
+++ lib/Finance/ID/KSEI.pm
@@ -8,11 +8,11 @@ package Finance::ID::KSEI;
 use 5.010001;
 use strict;
 use warnings;
+use Log::ger;
 
 use Exporter qw(import);
 our @EXPORT_OK = qw(
                        get_ksei_sec_ownership
-                       get_ksei_sec_data
                );
 
 our %SPEC;
@@ -22,12 +22,65 @@ $SPEC{':package'} = {
     summary => 'Get information from KSEI (Kustodian Sentral Efek Indonesia) (Indonesian Central Securities Depository)',
 };
 
+$SPEC{get_ksei_sec_ownership_url} = {
+    v => 1.1,
+    summary => 'Get KSEI securities ownership information',
+    description => <<'_',
+
+KSEI provides this in the form of monthly ZIP file. This function will just try
+to search the URL and return it.
+
+_
+    args => {
+        year => {
+            schema => 'date::year*',
+            req => 1,
+            pos => 0,
+        },
+        month => {
+            schema => 'date::month_num*',
+            req => 1,
+            pos => 1,
+        },
+    },
+};
+sub get_ksei_sec_ownership_url {
+    require DateTime;
+    require HTTP::Tiny;
+
+    my %args = @_;
+
+    # get the last weekday of the month
+    my $dt = DateTime->new(year => $args{year}, month => $args{month}, day => 1);
+    $dt->add(months => 1)->subtract(days => 1);
+    while ($dt->day_of_week > 5) { $dt->subtract(days => 1) }
+
+    # it may be a holiday, we don't consult Calendar::Indonesian::Holiday
+    # because IDX holiday might be slightly different. so we just try to probe
+    # for the URL for two weeks before giving up.
+    for (1..14) {
+        next if $dt->day_of_week > 5;
+        my $url = sprintf(
+            "https://www.ksei.co.id/Download/BalanceposEfek%04d%02d%02d.zip",
+            $dt->year, $dt->month, $dt->day,
+        );
+        log_trace "Trying $url ...";
+        my $res = HTTP::Tiny->new->head($url);
+        if ($res->{status} == 404) {
+            next;
+        } elsif ($res->{status} == 200) {
+            return [200, "OK", $url];
+        } else {
+            return [500, "Failed when trying URL '$url': $res->{status} - $res->{reason}"];
+        }
+    }
+    return [500, "Giving up after not finding the URL for 14 dates"];
+}
+
 1;
 # ABSTRACT:
 
 =head1 DESCRIPTION
-
-Not yet implemented, name grab only.
 
 
 =head1 SEE ALSO
